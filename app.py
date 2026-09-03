9594f97fd6f6df556a1a9e4645f92314e8a94383
seunghun04 <ok84638@gmail.com>
2026-09-03 10:55:33 +0900

    change page design

--- app.py
+++ app.py
@@ -1,20 +1,18 @@
-from flask import Flask, render_template   
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+@app.route("/")
 @app.route("/main")
 def main():
-	return render_template("main.html")
+    return render_template("main.html")
+
 
 @app.route("/about")
 def about():
-	return render_template("about.html")
-
-@app.route("/index")
-def index():
-	return render_template("index.html")
+    return render_template("about.html")
 
 
-
-if __name__ == "__main__" :
-	app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True) 
